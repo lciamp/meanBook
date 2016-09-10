6a6f01d1767042c9bbd31f6d1e5af73dc7ce3ad3
Louis Ciampanelli <louisciampanelli@Louiss-MBP.fios-router.home>
2016-09-10 17:42:08 -0400

    defining multiple routes

--- expMiddlewareAndParams/server.js
+++ expMiddlewareAndParams/server.js
@@ -9,6 +9,7 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
+
 // create routes for admin section
 
 // get an instance of the router
@@ -16,6 +17,7 @@ var adminRouter = express.Router();
 
 // route middlewear that will happen on every request
 adminRouter.use(function(req, res, next){
+
   //log each request to the console
   console.log(req.method, req.url);
 
@@ -28,9 +30,24 @@ adminRouter.get('/', function(req, res){
   res.send('I am the dashboard');
 });
 
-// user page 1337/admin/users
-adminRouter.get('/users', function(req, res){
-  res.send('I show all the users');
+// route middlewear to validate :name
+adminRouter.param('name', function(req, res, next, name){
+  // do validation here
+  // validation
+  // log something so we know its working
+  console.log('doing name validation on ' + name);
+
+  // once validation is done save the new iten in the req
+  req.name = name;
+
+  // go to the next thing
+  next();
+
+});
+
+// user page 1337/admin/users  *** WITH PARAMATERS
+adminRouter.get('/users/:name', function(req, res){
+  res.send('hello ' + req.name + '!');
 });
   
 // post page 1337/admin/posts
@@ -40,6 +57,7 @@ adminRouter.get('/posts', function(req, res){
 
 // add router to the app
 app.use('/admin', adminRouter);
+
 // start the server
 app.listen(1337);
 console.log('1337 is the magic port');
