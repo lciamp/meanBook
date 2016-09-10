c824fde6869b1938bb6df92fb42b0a9b53b34e76
Louis Ciampanelli <louisciampanelli@Louiss-MBP.fios-router.home>
2016-09-10 15:11:28 -0400

    added quotes

--- CH1/server.js
+++ CH1/server.js
@@ -1,21 +1,21 @@
 // get the http and filesystem modules
 var http = require('http'),
-	fs = require('fs');
+  fs = require('fs');
 
 // create our server using the http module
 http.createServer(function(req, res) {
 
-	// write to our server. set config for the response
-	res.writeHead(200, {
-		'Content-Type' : 'text/html',
-		'Access-Control-Allow-Orogon' : '*'
-	});
+  // write to our server. set config for the response
+  res.writeHead(200, {
+    'Content-Type' : 'text/html',
+    'Access-Control-Allow-Orogon' : '*'
+  });
 
-	// grab the index.html file using fs
-	var readStream = fs.createReadStream(__dirname + '/index.html');
+  // grab the index.html file using fs
+  var readStream = fs.createReadStream(__dirname + '/index.html');
 
-	// send the index.html file to our user
-	readStream.pipe(res);
+  // send the index.html file to our user
+  readStream.pipe(res);
 	
 }).listen(1337);
 
