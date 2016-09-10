4fc29313f518cbe2ef8f1d1b7c2cfac9c7f94e14
Louis Ciampanelli <louisciampanelli@Louiss-MBP.fios-router.home>
2016-09-10 16:18:48 -0400

    express router

---
+++ expRouter/server.js
@@ -0,0 +1,35 @@
+// load express package and creat our app
+var express = require('express'),
+  app = express(),
+  path = require('path');
+
+// send our index.html file to the user for the home page
+// send our index.html file to the user for the home page
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+// create routes for admin section
+
+// get an instance of the router
+var adminRouter = express.Router();
+
+// admin main page. the dashboard 1337/admin
+adminRouter.get('/', function(req, res){
+  res.send('I am the dashboard');
+});
+
+// user page 1337/admin/users
+adminRouter.get('/users', function(req, res){
+  res.send('I show all the users');
+});
+  
+// post page 1337/admin/posts
+adminRouter.get('/posts', function(req, res){
+  res.send('I show all the posts');
+});
+
+app.use('/admin', adminRouter);
+// start the server
+app.listen(1337);
+console.log('1337 is the magic port');
